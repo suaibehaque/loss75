85057f8ca05444d58c27a72ae0bf339cdb2e6c3f
suaibehaque <69385704+suaibehaque@users.noreply.github.com>
2021-04-16 22:41:56 +0600

    Add files via upload

---
+++ puzzle/2nd part puzzle/javascript/home design.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+ 
+ $( ".story" ).click(function() {
+  $(".all_contents").css({"display": "none"}),
+  $(".poem_menu").css({"display": "none"}),
+  $(".story_menu").css({"display": "block"}),
+  $(".jokes_menu").css({"display": "none"}),
+  $(".puzzle_menu").css({"display": "none"}),
+  $(".quote_menu").css({"display": "none"}),
+  $(".rhythm_menu").css({"display": "none"})
+});
+
+$( ".poem" ).click(function() {
+  $(".all_contents").css({"display": "none"}),
+  $(".story_menu").css({"display": "none"}),
+  $(".poem_menu").css({"display": "block"}),
+   $(".jokes_menu").css({"display": "none"}),
+   $(".puzzle_menu").css({"display": "none"}),
+   $(".quote_menu").css({"display": "none"}),
+  $(".rhythm_menu").css({"display": "none"})
+  
+});
+
+$( ".jokes" ).click(function() {
+  $(".all_contents").css({"display": "none"}),
+  $(".poem_menu").css({"display": "none"}),
+  $(".story_menu").css({"display": "none"}),
+  $(".jokes_menu").css({"display": "block"}),
+  $(".puzzle_menu").css({"display": "none"}),
+  $(".quote_menu").css({"display": "none"}),
+  $(".rhythm_menu").css({"display": "none"})
+});
+
+$( ".puzzle" ).click(function() {
+  $(".all_contents").css({"display": "none"}),
+  $(".poem_menu").css({"display": "none"}),
+  $(".story_menu").css({"display": "none"}),
+  $(".jokes_menu").css({"display": "none"}),
+  $(".puzzle_menu").css({"display": "block"}),
+  $(".quote_menu").css({"display": "none"}),
+  $(".rhythm_menu").css({"display": "none"})
+});
+ 
+$( ".rhythm" ).click(function() {
+  $(".all_contents").css({"display": "none"}),
+  $(".poem_menu").css({"display": "none"}),
+  $(".story_menu").css({"display": "none"}),
+  $(".jokes_menu").css({"display": "none"}),
+  $(".puzzle_menu").css({"display": "none"}),
+  $(".rhythm_menu").css({"display": "block"}),
+  $(".quote_menu").css({"display": "none"})
+  
+});
+ 
+ $( ".quote" ).click(function() {
+  $(".all_contents").css({"display": "none"}),
+  $(".poem_menu").css({"display": "none"}),
+  $(".story_menu").css({"display": "none"}),
+  $(".jokes_menu").css({"display": "none"}),
+  $(".puzzle_menu").css({"display": "none"}),
+  $(".rhythm_menu").css({"display": "none"}),
+  $(".quote_menu").css({"display": "block"})
+  
+});
+$( ".home_menu_nav" ).click(function() {
+  $(".all_contents").css({"display": "block"}),
+  $(".poem_menu").css({"display": "block"}),
+  $(".story_menu").css({"display": "block"}),
+  $(".jokes_menu").css({"display": "block"}),
+  $(".puzzle_menu").css({"display": "block"}),
+  $(".rhythm_menu").css({"display": "block"}),
+  $(".quote_menu").css({"display": "block"})
+  
+});
+ 
+ 
+ 
+});
+
+ 
